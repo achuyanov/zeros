29063b3ac45b830bcf5470b2ec03174ef4297fe7
AndreiChuyanov <achuyanov@gmail.com>
2019-10-02 20:24:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function zeros(expression) {
   // your solution
-}
+    let factArray = expression.split('*');
+    let ret2 = 0;
+    let ret5 = 0;
+  
+    const numK = function (val, k) {
+      let ret = 0;
+      while (val % k == 0) {
+        ret += 1;
+        val /= k;
+      }
+      return ret;
+    }
+
+    factArray.forEach (fact => {
+      let num = fact.replace(/!/g, '');
+      let step = (fact.includes('!!')) ? 2:1;
+      for (num; num >0; num -=step) {
+          ret5 += numK(num,5);
+          ret2 += numK(num,2);
+      } 
+    });
+    return (ret5 < ret2) ? ret5 : ret2;
+  }
